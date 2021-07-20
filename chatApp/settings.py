8e0568a27ffda3fd66aad5113838e43cd91fba5a
James254 <jemkiarie1@gmail.com>
2021-07-20 07:44:48 -0700

    changes

--- chatApp/settings.py
+++ chatApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6u*h^$w1ielwjw%797zzr8**hmbh@!_mc(f7p@qh6x$e69zufy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # Will Turn  False when ready for deployment
 
-ALLOWED_HOSTS = ['192.168.0.1:4000']
+ALLOWED_HOSTS = ['192.168.0.1:4000', '127.0.0.1']
               
 
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'markdown_deux',
+    #'markdown_deux',
     'chat',
     'accounts',
 ]
@@ -122,9 +122,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 # Simplified static file serving.
